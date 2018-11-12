00ee1a9f492b4ea3212427ee30c24d2b88f0c82e
tarap4ik <43042816+tarap4ik@users.noreply.github.com>
2018-11-12 22:12:20 +0300

    Update .bottom.menu.php

--- partnerskiy-kabinet/.bottom.menu.php
+++ partnerskiy-kabinet/.bottom.menu.php
@@ -1,13 +1,6 @@
 <?
 $aMenuLinks = Array(
 	Array(
-		"Новая страница", 
-		"/partnerskiy-kabinet/novaya-stranitsa.php", 
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
 		"Партнерский кабинет", 
 		"/partnerskiy-kabinet/", 
 		Array(), 
